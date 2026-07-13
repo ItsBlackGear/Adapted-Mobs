aff8118b4c787b7a16a759a9f0d82c047a0efbf4
evanbones <74276616+evanbones@users.noreply.github.com>
2026-07-13 10:40:26 -0700

    adjust effect colors

--- common/src/main/java/com/cf28/adaptedmobs/common/registries/AMMobEffects.java
+++ common/src/main/java/com/cf28/adaptedmobs/common/registries/AMMobEffects.java
@@ -13,22 +13,22 @@ public class AMMobEffects {
     public static final CoreRegistry<MobEffect> REGISTRIES = CoreRegistry.create(Registries.MOB_EFFECT, AdaptedMobs.MOD_ID);
 
     public static final Holder<MobEffect> SUPPORT_SPEED = REGISTRIES.holder("support_speed", () ->
-            new MobEffect(MobEffectCategory.BENEFICIAL, 0xFFFF00) {
+            new MobEffect(MobEffectCategory.BENEFICIAL, 0xD7A329) {
             }
                     .addAttributeModifier(Attributes.MOVEMENT_SPEED, AdaptedMobs.resource("support_speed"), 0.2, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
 
     public static final Holder<MobEffect> SUPPORT_STRENGTH = REGISTRIES.holder("support_strength", () ->
-            new MobEffect(MobEffectCategory.BENEFICIAL, 0xFF0000) {
+            new MobEffect(MobEffectCategory.BENEFICIAL, 0xA64022) {
             }
                     .addAttributeModifier(Attributes.ATTACK_DAMAGE, AdaptedMobs.resource("support_strength"), 3.0, AttributeModifier.Operation.ADD_VALUE));
 
     public static final Holder<MobEffect> SUPPORT_SLOWNESS = REGISTRIES.holder("support_slowness", () ->
-            new MobEffect(MobEffectCategory.HARMFUL, 0x0000FF) {
+            new MobEffect(MobEffectCategory.HARMFUL, 0x518382) {
             }
                     .addAttributeModifier(Attributes.MOVEMENT_SPEED, AdaptedMobs.resource("support_slowness"), -0.15, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
 
     public static final Holder<MobEffect> SUPPORT_WEAKNESS = REGISTRIES.holder("support_weakness", () ->
-            new MobEffect(MobEffectCategory.HARMFUL, 0x808080) {
+            new MobEffect(MobEffectCategory.HARMFUL, 0x5A696D) {
             }
                     .addAttributeModifier(Attributes.ATTACK_DAMAGE, AdaptedMobs.resource("support_weakness"), -4.0, AttributeModifier.Operation.ADD_VALUE));
 }
